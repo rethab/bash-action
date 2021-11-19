a47497fe52217ed5cc937b05e17a6c2f3fd214b0
Reto Hablützel <rethab@protonmail.ch>
2021-11-19 08:43:26 +0100

    add matcher and script

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+command="$1"
+who="$2"
+
+if [[ $command == "greet" ]]; then
+  echo "hello, $who"
+elif [[ $command == "error" ]]; then
+  echo "ERROR: script.sh, line 11: this is not good"
+fi
