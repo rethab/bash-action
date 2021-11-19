1dedb11519b8a525926cadc93f0cc36dc4ca7296
Reto Hablützel <rethab@protonmail.ch>
2021-11-19 08:53:13 +0100

    fix regex

--- script.sh
+++ script.sh
@@ -8,6 +8,6 @@ who="$2"
 if [[ $command == "greet" ]]; then
   echo "hello, $who"
 elif [[ $command == "error" ]]; then
-  echo "ERROR: script.sh, line 11: this is not good"
+  echo "ERROR: script.sh, line 11: this is not so good"
   exit 1
 fi
